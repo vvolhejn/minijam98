2b983218b7f5c6f5e1201572a44f7942a4e4bf82
Julia Bazinska <julia@bazinski.com>
2022-01-21 14:36:02 +0100

    Fix comments

--- src/scenes/levelScene.ts
+++ src/scenes/levelScene.ts
@@ -40,34 +40,8 @@ export class LevelScene extends Phaser.Scene {
         this.platforms.create(50, 250, 'ground');
         this.platforms.create(750, 220, 'ground');
 
-        // The this.player and its settings
-
+        // Create player
         this.player = new Player(this);
-        // this.player = this.physics.add.sprite(100, 450, 'dude');
-        // //  Player physics properties. Give the little guy a slight bounce.
-        // this.player.setBounce(0.2);
-        // this.player.setCollideWorldBounds(true);
-        //
-        // //  Our this.player animations, turning, walking left and walking right.
-        // this.anims.create({
-        //     key: 'left',
-        //     frames: this.anims.generateFrameNumbers('dude', {start: 0, end: 3}),
-        //     frameRate: 10,
-        //     repeat: -1
-        // });
-        //
-        // this.anims.create({
-        //     key: 'turn',
-        //     frames: [{key: 'dude', frame: 4}],
-        //     frameRate: 20
-        // });
-        //
-        // this.anims.create({
-        //     key: 'right',
-        //     frames: this.anims.generateFrameNumbers('dude', {start: 5, end: 8}),
-        //     frameRate: 10,
-        //     repeat: -1
-        // });
 
         //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
         this.stars = this.physics.add.group({
