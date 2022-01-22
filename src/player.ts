b58992a13ea41417372b27c978602c6357656042
Václav Volhejn <vaclav.volhejn@gmail.com>
2022-01-22 17:25:51 +0100

    Fix player

--- src/player.ts
+++ src/player.ts
@@ -1,6 +1,6 @@
 import { Fire } from "./fire";
 
-export const MAX_VELOCITY_X: integer = 160;
+export const MAX_VELOCITY_X: integer = 250;
 
 // horizontal speed is multiplied by (1 - FRICTION_COEF) each second
 // so values between 0 and 1 are reasonable
@@ -17,17 +17,24 @@ export abstract class Player extends Phaser.GameObjects.Container {
     ON_DAMAGE_VELOCITY_X = 300;
     ON_DAMAGE_VELOCITY_Y = 100;
     INVINCIBILITY_TIME_MS = 500;
+
+    // CURRENTLY UNUSED!
     invincible = false;  // briefly true after damage
 
     constructor(scene: Phaser.Scene, x: integer, y: integer, spriteKey: string) {
         super(scene);
         this.spriteKey = spriteKey;
         this.sprite = scene.physics.add.sprite(x, y, spriteKey);
-        this.sprite.setCollideWorldBounds(true);
 
         this.LEFT_ANIM_KEY = `${spriteKey}_left`;
         this.RIGHT_ANIM_KEY = `${spriteKey}_right`;
         this.TURN_ANIM_KEY = `${spriteKey}_turn`;
+    }
+
+    public setPhysicsProperties() {
+        // needs to be called after the sprite is added to the physics group
+        this.sprite.setCollideWorldBounds(true);
+        this.sprite.setMaxVelocity(MAX_VELOCITY_X, 100000);
     }
 
     public update(_time, delta): void {
@@ -45,7 +52,7 @@ export abstract class Player extends Phaser.GameObjects.Container {
         this.sprite.setVelocityY(-this.ON_DAMAGE_VELOCITY_Y);
 
         this.sprite.setTint(0xFF0000);
-        this.invincible = true;
+        // this.invincible = true;
         // serol.alpha = 0.5;
         scene.time.addEvent({
             delay: this.INVINCIBILITY_TIME_MS,
