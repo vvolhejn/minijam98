2cf3a5d339654ba795a5ea8f31fe43a96b962836
Julia Bazinska <julia@bazinski.com>
2022-01-23 17:14:01 +0100

    fucking fires

--- src/scenes/levelScene.ts
+++ src/scenes/levelScene.ts
@@ -273,7 +273,7 @@ export class LevelScene extends Phaser.Scene {
 
         // Fires.
         map.getObjectLayer('fires')?.objects.forEach((fireTile) => {
-            let fire = new Fire(this, offsetX + fireTile.x + 15, offsetY + fireTile.y - 40, 'fire');
+            let fire = new Fire(this, offsetX + fireTile.x + 15, offsetY + fireTile.y - 38, 'fire');
             this.fires.add(fire, true);
             fire.body.setSize(30, 60, true);
             fire.updateScale();
