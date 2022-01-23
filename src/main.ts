4d27ee184de6ee692ccb34f9c07922da20de5e44
Václav Volhejn <vaclav.volhejn@gmail.com>
2022-01-23 18:00:22 +0100

    Remove debug mode

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ const gameConfig = {
         default: 'arcade',
         arcade: {
             gravity: {y: 800},
-            debug: true,
+            debug: false,
             fps: 60, // FPS of the physics simulation - maybe higher could increase stability
             timeScale: 1, // higher to slow down - for debugging
         }
