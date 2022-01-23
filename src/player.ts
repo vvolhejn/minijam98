d93e5e059b58a9a410252da16860b686fccd92bf
Roman <r.sobkuliak@gmail.com>
2022-01-23 22:47:48 +0100

    Player fire collision decreases time

--- src/player.ts
+++ src/player.ts
@@ -1,10 +1,12 @@
 import { Fire } from "./fire";
+import { LevelScene } from "./scenes/levelScene";
 
 export const MAX_VELOCITY_X: integer = 250;
 
 // horizontal speed is multiplied by (1 - FRICTION_COEF) each second
 // so values between 0 and 1 are reasonable
 const FRICTION_COEF: number = 0.85;
+const FIRE_PLAYER_COLLISION_PENALTY_MS = 2000;
 
 export abstract class Player extends Phaser.GameObjects.Container {
     sprite: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
@@ -44,13 +46,15 @@ export abstract class Player extends Phaser.GameObjects.Container {
         );
     }
 
-    public onFireCollision(fire: Fire, scene) {
+    public onFireCollision(fire: Fire, scene : LevelScene) {
         if (this.invincible) return;
 
         const positionDiff = this.sprite.getCenter().clone().subtract(fire.getCenter());
         this.sprite.setVelocityX(this.ON_DAMAGE_VELOCITY_X * (positionDiff.x > 0 ? 1 : (-1)));
         this.sprite.setVelocityY(-this.ON_DAMAGE_VELOCITY_Y);
 
+        console.log(scene.timer.total_ms);
+        scene.timer.total_ms = Math.max(scene.timer.total_ms - FIRE_PLAYER_COLLISION_PENALTY_MS, 0);
         this.sprite.setTint(0xFF0000);
         // this.invincible = true;
         // serol.alpha = 0.5;
